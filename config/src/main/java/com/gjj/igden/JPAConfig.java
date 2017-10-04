a7f9e735d175a2e5443e6b49a51f72b4c0639a01
martinchukachinaka <martinchukachinaka@gmail.com>
2017-10-04 04:37:49 +0100

    Added the ability to work with JNDI as a datasource if configured

--- config/src/main/java/com/gjj/igden/JPAConfig.java
+++ config/src/main/java/com/gjj/igden/JPAConfig.java
@@ -12,19 +12,21 @@ import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jndi.JndiTemplate;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.naming.NamingException;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackages ="com.gjj.igden.dao")
+@EnableJpaRepositories(basePackages = "com.gjj.igden.dao")
 @ComponentScan({"com.gjj.igden.dao", "com.gjj.igden.service"})
 @PropertySource("classpath:config.properties")
 public class JPAConfig {
@@ -44,19 +46,34 @@ public class JPAConfig {
 
     private static final String HIBERNATE_DIALECT = "hibernate.dialect";
 
+    private static final String JNDI_URL = "jndi.url";
+
+    private static final String USE_JNDI = "use.jndi";
+
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
 
+
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource datasource = new DriverManagerDataSource();
-        datasource.setDriverClassName(env.getProperty(DRIVER_CLASS_NAME));
-        datasource.setUrl(env.getProperty(DATABASE_URL));
-        datasource.setUsername(env.getProperty(USERNAME));
-        datasource.setPassword(env.getProperty(PASSWORD, StringUtils.EMPTY));
-        return datasource;
+        boolean useJndi = "true".equalsIgnoreCase(env.getProperty(USE_JNDI, "false"));
+        System.out.println("Using JNDI? " + useJndi);
+        if (useJndi) {
+            try {
+                return (DataSource) new JndiTemplate().lookup(env.getProperty(JNDI_URL));
+            } catch (NamingException ex) {
+                throw new IllegalStateException("Problem encountered while connecting to JNDI. Message: " + ex.getMessage());
+            }
+        } else {
+            DriverManagerDataSource datasource = new DriverManagerDataSource();
+            datasource.setDriverClassName(env.getProperty(DRIVER_CLASS_NAME));
+            datasource.setUrl(env.getProperty(DATABASE_URL));
+            datasource.setUsername(env.getProperty(USERNAME));
+            datasource.setPassword(env.getProperty(PASSWORD, StringUtils.EMPTY));
+            return datasource;
+        }
     }
 
     @Bean
